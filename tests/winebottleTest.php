52b2e48a456be83cb418fdd3ebfdc1622b90bc28
Anita Garcia <anitagarcia819@gmail.com>
2016-11-03 23:14:02 -0700

    modified test files to make circleci test pass

--- tests/winebottleTest.php
+++ tests/winebottleTest.php
@@ -1,7 +1,7 @@
 <?php
 use PHPUnit\Framework\TestCase;
-//include_once("lib/wineBottle.php");
-include_once("../lib/wineBottle.php");
+include_once("lib/wineBottle.php");
+//nclude_once("../lib/wineBottle.php");
 
 class BottleTest extends TestCase
 {
