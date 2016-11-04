f6954427bfceb199cbb4f5c9d91fe6e49d132414
Anita Garcia <anitagarcia819@gmail.com>
2016-11-03 22:30:41 -0700

    Updated AWS RDS credentials

--- api/db.include.php
+++ api/db.include.php
@@ -1,13 +1,10 @@
 <?php
     function getDatabaseConnection() {
         //echo "DB";
-        //$host = "wc-database.cvuylrrqda7p.us-west-1.rds.amazonaws.com";
+        $host = "wc-database.cvuylrrqda7p.us-west-1.rds.amazonaws.com";
         $dbname = "wc_db";
-        //$username = "wc_user";
-        //$password = "wc4tw!123";
-        $host = "localhost";
-        $username = "root";
-        $password = "root";
+        $username = "wc_user";
+        $password = "wc4tw!123";
         try{
                 $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
                 $conn = $dbConn;
--- lib/database.php
+++ lib/database.php
@@ -6,10 +6,10 @@ class Database {
            
 public static function getInstance() {
         //hardcoded values need to change later 
-        $host = "localhost";
+        $host = "wc-database.cvuylrrqda7p.us-west-1.rds.amazonaws.com";
         $dbname = "wc_db";
-        $username = "root";
-        $password = "root";
+        $username = "wc_user";
+        $password = "wc4tw!123";
         if(!isset(self::$instance)){
             $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
             self::$instance = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
